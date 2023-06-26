41c4396c480622f3b3455730002db1b80c055a12
gyasi.eth <104705433+barigyasi@users.noreply.github.com>
2023-06-26 13:19:25 -0500

    header changes

--- src/components/Game/jttm.js
+++ src/components/Game/jttm.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Jttm = () => {
   return (
     <>
-    <iframe frameborder="0" src="https://itch.io/embed-upload/8173177?color=333333" allowfullscreen="" width="640" height="380"><a href="https://metaphysicalart.itch.io/mothershipgame">Play Journey to the Mothership! on itch.io</a></iframe>
+    <iframe frameborder="0" src="https://itch.io/embed-upload/8178075?color=333333" allowfullscreen="" width="800" height="620"><a href="https://metaphysicalart.itch.io/mothershipgame">Play Journey to the Mothership! </a></iframe>
     </>
   )
 }
