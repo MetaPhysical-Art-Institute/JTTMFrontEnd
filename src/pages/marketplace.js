496b56b9853b1b35a8de85d22450639143b277d6
gyasi.eth <104705433+barigyasi@users.noreply.github.com>
2023-06-26 22:33:48 -0500

    testing

---
+++ src/pages/marketplace.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Listings from '../components/Marketplace/market'
+
+const Marketplace = () => {
+  return (
+    <>
+    <div>
+        <Listings />
+    </div>
+    </>
+  )
+}
+
+export default Marketplace
