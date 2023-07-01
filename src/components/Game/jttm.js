1bc60cabab8c71a7a9a93e65a7a8ed04fce87f31
gyasi.eth <104705433+barigyasi@users.noreply.github.com>
2023-07-01 16:29:47 -0500

    game version update v2.1

--- src/components/Game/jttm.js
+++ src/components/Game/jttm.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Jttm = () => {
   return (
     <>
-<iframe frameborder="0" src="https://itch.io/embed-upload/8195274?color=333333" allowfullscreen="" width="800" height="620"><a href="https://metaphysicalart.itch.io/mothershipgame">Play Journey to the Mothership! </a></iframe>    </>
+    <iframe frameborder="0" src="https://itch.io/embed-upload/8210620?color=333333" allowfullscreen="" width="800" height="620"><a href="https://metaphysicalart.itch.io/mothershipgame">Play Journey to the Mothership!</a></iframe>
+   </>
   )
 }
 
