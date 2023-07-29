e7335ad3ec22f44a2f8266f108a2d354bd7a2bfa
gyasi.eth <104705433+barigyasi@users.noreply.github.com>
2023-07-29 05:03:10 -0500

    iframe updated

--- src/components/Game/jttm.js
+++ src/components/Game/jttm.js
@@ -4,11 +4,23 @@ const Jttm = () => {
   return (
     <>
     <div className='game'>
-    <iframe frameborder="0" src="https://itch.io/embed/2134447" width="552" height="167"><a href="https://metaphysicalart.itch.io/mothershipgame">Journey to the Mothership! by MetaPhysicalArt</a></iframe>
-    <br></br>
+    <iframe frameborder="0" src="https://itch.io/embed-upload/8405248?color=333333" allowfullscreen="" width="800" height="620"><a href="https://metaphysicalart.itch.io/mothershipgame">Play Journey to the Mothership! </a></iframe>   
+     <br></br>
     <p>Local Wallet Transfer Enabled</p>
     <br></br>
     <p>Transactions on the blockchain can sometimes take a moment, please allow time to claim tokens and the teleporter to appear! Thanks!</p>
+    <br></br>
+    Instructions:<br></br>
+- Move your character using the W, A, S,and D keys.<br></br>
+- Aim and shoot using the mouse.<br></br>
+- Press T to draw your weapon and I to access your inventory.<br></br>
+- Each level begins with full health.<br></br>
+- Defeat enemies to progress through the level.<br></br>
+- Once the level is cleared collect RGT tokens from the ATM to advance to the next level.<br></br>
+- Reach the Mothership to complete the game.<br></br>
+<br></br><br></br>
+Good luck on your journey to the Mothership! 
+
    </div>
    </>
   )
