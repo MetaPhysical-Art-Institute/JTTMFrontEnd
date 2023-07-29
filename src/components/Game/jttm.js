dafd0b8e4611558ec485ec96cf9acab43ce2bf63
gyasi.eth <104705433+barigyasi@users.noreply.github.com>
2023-07-29 04:58:05 -0500

    v2.5 Jttm

--- src/components/Game/jttm.js
+++ src/components/Game/jttm.js
@@ -4,9 +4,9 @@ const Jttm = () => {
   return (
     <>
     <div className='game'>
-      <iframe frameborder="0" src="https://itch.io/embed-upload/8233950?color=333333" allowfullscreen="" width="800" height="620"><a href="https://metaphysicalart.itch.io/mothershipgame">Play Journey to the Mothership! </a></iframe>
+    <iframe frameborder="0" src="https://itch.io/embed/2134447" width="552" height="167"><a href="https://metaphysicalart.itch.io/mothershipgame">Journey to the Mothership! by MetaPhysicalArt</a></iframe>
     <br></br>
-    <p>Warning: Local wallet allows you to connect using only a password and play the game; however we have not enabled local wallet exports from the game at this time. This means your NFT's and Roganite tokens will be locked inside the game until we are able to enable withdrawal! Use Metamask or another major wallet to ensure proper NFT transfer in the future! Thanks!</p>
+    <p>Local Wallet Transfer Enabled</p>
     <br></br>
     <p>Transactions on the blockchain can sometimes take a moment, please allow time to claim tokens and the teleporter to appear! Thanks!</p>
    </div>
