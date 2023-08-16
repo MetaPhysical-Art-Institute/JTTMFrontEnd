6949c1a3a656e6fe5c09b5c39ef6fcd38ea0c664
gyasi.eth <104705433+barigyasi@users.noreply.github.com>
2023-08-15 23:04:26 -0500

    more css updates

--- src/pages/home.js
+++ src/pages/home.js
@@ -184,9 +184,9 @@ function Home() {
     <div className='member'>
       <p>You gotta play to get in! </p>
       <br></br>
-      
+      <div className='video'>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/ZE1ROakhSeM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-   
+      </div>
          
       <center>
       <ConnectWallet />
